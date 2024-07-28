5c19e9723553a28b4771c37516481f4c8ab67081
theMirmakhmudov <mr.mirmakhmudov16112008@gmail.com>
2024-07-29 00:00:49 +0500

    Commit 1) Class

---
+++ 1)Class.py
@@ -0,0 +1,16 @@
+# Obyektlarni yaratish uchun shablon.  # noqa
+# https://github.com/theMirmakhmudov
+# https://youtube.com/@Mirmakhmudov_coder # noqa
+
+class Book:
+    def __init__(self, book_name, book_id, book_author):
+        self.book_name = book_name
+        self.book_id = book_id
+        self.book_author = book_author
+
+    def say_book_name(self):
+        return f"Book name is {self.book_name}"
+
+
+fairy_tale_book = Book("Harry Potter and secret room", 1, "Joanna Ketlin Rouling")
+print(fairy_tale_book.say_book_name())
